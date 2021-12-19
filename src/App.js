f5cbbc6f588c5ee62b5e5ad337bfc30093390642
Danny <Danny@Apples-iMac.local>
2021-12-20 10:30:41 +1300

    record image spinning on homepage

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <img src="record-transparent.png" className="App-logo" alt="spinning record" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
