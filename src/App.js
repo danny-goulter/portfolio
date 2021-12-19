f0c080f8b67db2b1734d3dd5ddfa5db1f444289f
Danny <Danny@Apples-iMac.local>
2021-12-20 11:48:11 +1300

    building

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        <img src="record.png" onClick={playScratch()} className="App-logo" alt="spinning record" />
+        <img src="record.png" onClick={() => playScratch} className="App-logo" alt="spinning record" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
